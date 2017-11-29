fbe2296a8c360cf101a1a8126139edabfaca1b65
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-11-29 15:03:23 -0800

    add returning errors example

---
+++ examples/returning-errors.js
@@ -0,0 +1,34 @@
+
+import { struct, StructError } from 'superstruct'
+
+// Define a struct to validate with.
+const User = struct({
+  id: 'number',
+  name: 'string',
+  email: 'string',
+})
+
+// Define data to be validated.
+const data = {
+  id: 1,
+  name: true,
+  email: 'jane@example.com',
+}
+
+// Validate the data by calling `assert`. In this case the `name` property is
+// invalid, so a `property_invalid` error will be thrown.
+const result = User.validate(data)
+
+if (result instanceof StructError) {
+  console.error(result)
+} else {
+  console.log('Valid!')
+}
+
+// StructError: 'Expected the `name` property to be of type "string", but it was `false`.' {
+//   code: 'property_invalid',
+//   type: 'string',
+//   path: ['name'],
+//   key: 'name',
+//   value: false,
+// }
