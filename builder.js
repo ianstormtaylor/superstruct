f252bc19284c47fdc0a524bfb08cad351b4e235f
Luke Edwards <luke.edwards05@gmail.com>
2017-11-29 19:54:49 -0800

    replace Browserify + Babel —> Rollup + Bublé;
    - import my `builder` script
    - consolidate`build:*` commands
    - generate 3 export files: UMD, CJS, ESM
    - print GZIP size on every build

---
+++ builder.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const { rollup } = require('rollup');
+const { minify } = require('uglify-js');
+const pretty = require('pretty-bytes');
+const sizer = require('gzip-size');
+const pkg = require('./package');
+
+const umd = pkg['umd:main'];
+
+rollup({
+  input: 'src/index.js',
+  plugins: [
+    require('rollup-plugin-buble')({
+      objectAssign: 'Object.assign',
+      transforms: { modules:false }
+    }),
+    require('rollup-plugin-commonjs')({
+      sourceMap: false
+    }),
+    require('rollup-plugin-node-resolve')({
+      jsnext: true
+    })
+  ]
+}).then(bun => {
+  bun.write({
+    format: 'cjs',
+    file: pkg.main,
+    interop: false
+  });
+
+  bun.write({
+    format: 'es',
+    file: pkg.module,
+    interop: false
+  });
+
+  bun.write({
+    file: umd,
+    format: 'umd',
+    name: pkg.name,
+    interop: false
+  }).then(_ => {
+    const data = fs.readFileSync(umd, 'utf8');
+
+    // produce minified output
+    const { code } = minify(data);
+    fs.writeFileSync(umd, code);
+
+    // output gzip size
+    const int = sizer.sync(code);
+    console.log(`> gzip size: ${ pretty(int) }`);
+  });
+}).catch(err => console.log(err))
