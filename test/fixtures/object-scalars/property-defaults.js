6914be34c20bb7e3a6d113f5b4f9e43c4f3d24b1
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-12-10 03:07:13 -0500

    fix object defaults

---
+++ test/fixtures/object-scalars/property-defaults.js
@@ -0,0 +1,18 @@
+
+import { struct } from '../../..'
+
+export const Struct = struct({
+  name: 'string?',
+  age: 'number'
+}, {
+  name: 'unknown',
+})
+
+export const data = {
+  age: 42,
+}
+
+export const output = {
+  name: 'unknown',
+  age: 42,
+}
