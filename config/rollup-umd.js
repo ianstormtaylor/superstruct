1aa5857bc12d8df3bdf344068b0feb3224fe2d53
Yifeng Wang <ewind@mail.ustc.edu.cn>
2017-12-22 02:48:05 +0800

    Strip out NODE_ENV in UMD build (#71)
    * replace umd env
    
    * Update rollup-umd.js

--- config/rollup-umd.js
+++ config/rollup-umd.js
@@ -1,5 +1,10 @@
 
 import config from './rollup'
+import replace from 'rollup-plugin-replace'
+
+config.plugins.push(
+  replace({ 'process.env.NODE_ENV': JSON.stringify('development') })
+)
 
 config.output = {
   file: './umd/superstruct.js',
