d23231dc5cdb2de8f2327b86d27a146696877bd6
Luke Edwards <luke.edwards05@gmail.com>
2018-02-23 12:29:40 -0800

    Add static 'format' method on StructError (#95)
    * add static 'format' method on StructError
    
    * fix linting

--- src/error.js
+++ src/error.js
@@ -8,14 +8,14 @@
 class StructError extends TypeError {
 
   constructor(attrs) {
-    const { data, value, type, path, reason, errors = [] } = attrs
-    const message = `Expected a value of type \`${type}\`${path.length ? ` for \`${path.join('.')}\`` : ''} but received \`${JSON.stringify(value)}\`.`
-    super(message)
-    this.data = data
-    this.path = path
-    this.value = value
-    this.type = type
-    this.reason = reason
+    super()
+    const errors = attrs.errors || []
+    this.message = this.constructor.format(attrs)
+    this.data = attrs.data
+    this.path = attrs.path
+    this.value = attrs.value
+    this.reason = attrs.reason
+    this.type = attrs.type
     this.errors = errors
     if (!errors.length) errors.push(this)
     if (Error.captureStackTrace) {
@@ -23,6 +23,11 @@ class StructError extends TypeError {
     } else {
       this.stack = (new Error()).stack
     }
+  }
+
+  static format(attrs) {
+    const path = attrs.path.join('.')
+    return `Expected a value of type \`${attrs.type}\`${path.length ? ` for \`${path}\`` : ''} but received \`${JSON.stringify(attrs.value)}\`.`
   }
 
 }
