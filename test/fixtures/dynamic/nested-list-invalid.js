2f76bf3f560c2d365cbb6c1e4a0bdbcbd65ed0f2
DecentM <decentm@decentm.com>
2019-03-26 18:29:29 +0100

    Create tests for the `dynamic` kind, with nested lists

---
+++ test/fixtures/dynamic/nested-list-invalid.js
@@ -0,0 +1,59 @@
+import { struct } from '../../..'
+
+const Person = struct({
+  age: 'number',
+  nodes: [struct.lazy(() => struct.optional(node))],
+})
+
+const Product = struct({
+  price: 'string',
+})
+
+const map = {
+  PERSON: Person,
+  PRODUCT: Product,
+}
+
+const node = struct({
+  kind: struct.enum(Object.keys(map)),
+  options: struct.dynamic((value, parent) => {
+    return map[parent.kind] || struct('undefined')
+  }),
+})
+
+export const Struct = struct({
+  nodes: [node],
+})
+
+export const data = {
+  nodes: [
+    {
+      kind: 'PERSON',
+      options: {
+        age: 34,
+        nodes: [
+          {
+            kind: 'PERSON',
+            options: {
+              age: 23,
+              nodes: [],
+            },
+          },
+          {
+            kind: 'WHOOPS',
+            options: {
+              price: 'Only $19.99!',
+            },
+          },
+        ],
+      },
+    },
+  ],
+}
+
+export const error = {
+  path: ['nodes', 0, 'options', 'nodes', 1, 'kind'],
+  value: 'WHOOPS',
+  type: '{kind,options} | undefined',
+  reason: undefined,
+}
