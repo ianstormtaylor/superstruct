c7c9c4188a0965f9d303909c2275957d6fe69f83
Felix Gertz <dev@felixgertz.de>
2019-09-21 14:23:03 +0200

    Fix linting errors in test

--- test/fixtures/custom-error/custom-error.js
+++ test/fixtures/custom-error/custom-error.js
@@ -1,40 +1,42 @@
 import { superstruct } from '../../..'
 
 class StructErrorWithoutValue extends TypeError {
-    static format(attrs) {
-      const { type, path } = attrs;
-      const message = `Expected a value of type \`${type}\`${path.length ? ` for \`${path.join('.')}\`` : ''}.`;
-      return message;
-    }
-  
-    constructor(attrs) {
-      const message = StructErrorWithoutValue.format(attrs);
-      super(message);
-  
-      const { path, reason, type, errors = [] } = attrs;
-      this.path = path;
-      this.reason = reason;
-      this.type = type;
-      this.errors = errors.map(error => {
-        delete error.value;
-        delete error.data;
-        return error;
-      });
-  
-      if (!errors.length) {
-        errors.push(this);
-      }
-  
-      if (Error.captureStackTrace) {
-        Error.captureStackTrace(this, this.constructor);
-      } else {
-        this.stack = (new Error()).stack;
-      }
-    }
+  static format(attrs) {
+    const { type, path } = attrs
+    const message = `Expected a value of type \`${type}\`${
+      path.length ? ` for \`${path.join('.')}\`` : ''
+    }.`
+    return message
   }
 
+  constructor(attrs) {
+    const message = StructErrorWithoutValue.format(attrs)
+    super(message)
+
+    const { path, reason, type, errors = [] } = attrs
+    this.path = path
+    this.reason = reason
+    this.type = type
+    this.errors = errors.map(error => {
+      delete error.value
+      delete error.data
+      return error
+    })
+
+    if (!errors.length) {
+      errors.push(this)
+    }
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor)
+    } else {
+      this.stack = new Error().stack
+    }
+  }
+}
+
 const struct = superstruct({
-    error: StructErrorWithoutValue,
+  error: StructErrorWithoutValue,
 })
 
 export const Struct = struct('number')
