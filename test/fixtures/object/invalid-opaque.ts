321002ce7f52c62c245737a5e57a298140514721
Ian Storm Taylor <ian@ianstormtaylor.com>
2020-06-08 23:00:31 -0400

    add missing tests

---
+++ test/fixtures/object/invalid-opaque.ts
@@ -0,0 +1,12 @@
+import { object } from '../../..'
+
+export const Struct = object()
+
+export const data = 'invalid'
+
+export const error = {
+  value: 'invalid',
+  type: 'Object',
+  path: [],
+  branch: [data],
+}
---
+++ test/fixtures/object/valid-opaque.ts
@@ -0,0 +1,13 @@
+import { object } from '../../..'
+
+export const Struct = object()
+
+export const data = {
+  a: 'string',
+  b: 42,
+}
+
+export const output = {
+  a: 'string',
+  b: 42,
+}
