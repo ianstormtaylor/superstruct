4f3522387fceb447748535367ecb490d459445c6
kamijin_fanta <kamijin@live.jp>
2021-01-25 10:05:54 +0900

    utils

--- src/structs/utilities.ts
+++ src/structs/utilities.ts
@@ -1,7 +1,13 @@
 import { Struct, Context, Validator } from '../struct'
 import { object, optional } from './types'
-import { ObjectSchema, Assign, ObjectType, PartialObjectSchema } from '../utils'
-import { Error } from '../error'
+import {
+  ObjectSchema,
+  Assign,
+  ObjectType,
+  PartialObjectSchema,
+  ObjectError,
+} from '../utils'
+import { Error, ErrorDetail, TypeErrorDetail } from '../error'
 
 /**
  * Create a new struct that combines the properties properties from multiple
@@ -10,48 +16,71 @@ import { Error } from '../error'
  * Like JavaScript's `Object.assign` utility.
  */
 
-export function assign<A extends ObjectSchema, B extends ObjectSchema>(
-  A: Struct<ObjectType<A>, A>,
-  B: Struct<ObjectType<B>, B>
-): Struct<ObjectType<Assign<A, B>>, Assign<A, B>>
 export function assign<
   A extends ObjectSchema,
   B extends ObjectSchema,
-  C extends ObjectSchema
+  E1 extends Error,
+  E2 extends Error
 >(
-  A: Struct<ObjectType<A>, A>,
-  B: Struct<ObjectType<B>, B>,
-  C: Struct<ObjectType<C>, C>
-): Struct<ObjectType<Assign<Assign<A, B>, C>>, Assign<Assign<A, B>, C>>
+  A: Struct<ObjectType<A>, A, E1>,
+  B: Struct<ObjectType<B>, B, E2>
+): Struct<ObjectType<Assign<A, B>>, Assign<A, B>, E1 | E2>
 export function assign<
   A extends ObjectSchema,
   B extends ObjectSchema,
   C extends ObjectSchema,
-  D extends ObjectSchema
+  E1 extends Error,
+  E2 extends Error,
+  E3 extends Error
 >(
-  A: Struct<ObjectType<A>, A>,
-  B: Struct<ObjectType<B>, B>,
-  C: Struct<ObjectType<C>, C>,
-  D: Struct<ObjectType<D>, D>
+  A: Struct<ObjectType<A>, A, E1>,
+  B: Struct<ObjectType<B>, B, E2>,
+  C: Struct<ObjectType<C>, C, E3>
 ): Struct<
-  ObjectType<Assign<Assign<Assign<A, B>, C>, D>>,
-  Assign<Assign<Assign<A, B>, C>, D>
+  ObjectType<Assign<Assign<A, B>, C>>,
+  Assign<Assign<A, B>, C>,
+  E1 | E2 | E3
 >
 export function assign<
   A extends ObjectSchema,
   B extends ObjectSchema,
   C extends ObjectSchema,
   D extends ObjectSchema,
-  E extends ObjectSchema
+  E1 extends Error,
+  E2 extends Error,
+  E3 extends Error,
+  E4 extends Error
 >(
-  A: Struct<ObjectType<A>, A>,
-  B: Struct<ObjectType<B>, B>,
-  C: Struct<ObjectType<C>, C>,
-  D: Struct<ObjectType<D>, D>,
-  E: Struct<ObjectType<E>, E>
+  A: Struct<ObjectType<A>, A, E1>,
+  B: Struct<ObjectType<B>, B, E2>,
+  C: Struct<ObjectType<C>, C, E3>,
+  D: Struct<ObjectType<D>, D, E4>
+): Struct<
+  ObjectType<Assign<Assign<Assign<A, B>, C>, D>>,
+  Assign<Assign<Assign<A, B>, C>, D>,
+  E1 | E2 | E3 | E4
+>
+export function assign<
+  A extends ObjectSchema,
+  B extends ObjectSchema,
+  C extends ObjectSchema,
+  D extends ObjectSchema,
+  E extends ObjectSchema,
+  E1 extends Error,
+  E2 extends Error,
+  E3 extends Error,
+  E4 extends Error,
+  E5 extends Error
+>(
+  A: Struct<ObjectType<A>, A, E1>,
+  B: Struct<ObjectType<B>, B, E2>,
+  C: Struct<ObjectType<C>, C, E3>,
+  D: Struct<ObjectType<D>, D, E4>,
+  E: Struct<ObjectType<E>, E, E5>
 ): Struct<
   ObjectType<Assign<Assign<Assign<Assign<A, B>, C>, D>, E>>,
-  Assign<Assign<Assign<Assign<A, B>, C>, D>, E>
+  Assign<Assign<Assign<Assign<A, B>, C>, D>, E>,
+  E1 | E2 | E3 | E4 | E5
 >
 export function assign(...Structs: Struct<any>[]): any {
   const schemas = Structs.map((s) => s.schema)
@@ -78,9 +107,9 @@ export function define<T, E extends Error>(
  * validation logic that changes based on its input.
  */
 
-export function dynamic<T>(
-  fn: (value: unknown, ctx: Context) => Struct<T, any>
-): Struct<T, null> {
+export function dynamic<T, E extends Error>(
+  fn: (value: unknown, ctx: Context) => Struct<T, any, E>
+): Struct<T, null, E> {
   return new Struct({
     type: 'dynamic',
     schema: null,
@@ -108,8 +137,10 @@ export function dynamic<T>(
  * circular definition problem.
  */
 
-export function lazy<T>(fn: () => Struct<T, any>): Struct<T, null> {
-  let struct: Struct<T, any> | undefined
+export function lazy<T, E extends Error>(
+  fn: () => Struct<T, any, E>
+): Struct<T, null, E> {
+  let struct: Struct<T, any, E> | undefined
   return new Struct({
     type: 'lazy',
     schema: null,
@@ -136,9 +167,13 @@ export function lazy<T>(fn: () => Struct<T, any>): Struct<T, null> {
  */
 
 export function omit<S extends ObjectSchema, K extends keyof S>(
-  struct: Struct<ObjectType<S>, S>,
+  struct: Struct<ObjectType<S>, S, any>,
   keys: K[]
-): Struct<ObjectType<Omit<S, K>>, Omit<S, K>> {
+): Struct<
+  ObjectType<Omit<S, K>>,
+  Omit<S, K>,
+  ObjectError<S> | TypeErrorDetail
+> {
   const { schema } = struct
   const subschema: any = { ...schema }
 
@@ -179,7 +214,11 @@ export function partial<S extends ObjectSchema>(
 export function pick<S extends ObjectSchema, K extends keyof S>(
   struct: Struct<ObjectType<S>, S>,
   keys: K[]
-): Struct<ObjectType<Pick<S, K>>, Pick<S, K>> {
+): Struct<
+  ObjectType<Pick<S, K>>,
+  Pick<S, K>,
+  ObjectError<S> | TypeErrorDetail
+> {
   const { schema } = struct
   const subschema: any = {}
 
