b312997facc9e185893a4e6c16b59aa39e57e6f2
Ian Storm Taylor <ian@ianstormtaylor.com>
2021-03-28 11:41:20 -0400

    fix max refinement error message

--- src/structs/refinements.ts
+++ src/structs/refinements.ts
@@ -43,7 +43,7 @@ export function max<T extends number | Date, S extends any>(
     return exclusive
       ? value < threshold
       : value <= threshold ||
-          `Expected a ${struct.type} greater than ${
+          `Expected a ${struct.type} less than ${
             exclusive ? '' : 'or equal to '
           }${threshold} but received \`${value}\``
   })
