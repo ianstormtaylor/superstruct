f619f226c8b92138a389de467bd30c2d1910d7e8
Florent <44880941+cwouam@users.noreply.github.com>
2022-09-19 18:39:54 +0200

    Stop coercing empty optional fields to undefined values (#1106)

--- src/utils.ts
+++ src/utils.ts
@@ -175,7 +175,7 @@ export function* run<T, S>(
         } else if (value instanceof Set) {
           value.add(v)
         } else if (isObject(value)) {
-          value[k] = v
+          if (v !== undefined) value[k] = v
         }
       }
     }
