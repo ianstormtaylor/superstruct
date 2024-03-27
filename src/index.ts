7d8a2e15a32b38a7f971e53808d87cbeeaa2ddcd
Elliot Winkler <elliot.winkler@gmail.com>
2024-03-27 08:24:28 -0600

    Export ObjectSchema, Optionalize, Simplify (#17)
    We make use of these types in `@metamask/utils`. Exporting them allows
    us to upgrade this package to `@metamask/superstruct`.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './structs/coercions.js';
 export * from './structs/refinements.js';
 export * from './structs/types.js';
 export * from './structs/utilities.js';
+export type { ObjectSchema, Optionalize, Simplify } from './utils.js';
